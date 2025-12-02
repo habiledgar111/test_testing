c0f47813e2f600e05a64ccaac2db120e1026dc34
habil <habiedgar111@gmail.com>
2025-12-02 07:55:42 +0700

    add: adding maven build

---
+++ src/main/java/com/example/test_unit/TestUnitApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_unit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestUnitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestUnitApplication.class, args);
+	}
+
+}
